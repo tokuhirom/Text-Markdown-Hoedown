9c1aea05bd5d44a1cf76993639e5a22db8dc5191
tokuhirom <tokuhirom@gmail.com>
2013-09-30 20:06:26 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 29f004a..dcc5592 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Text-Markdown-Hoedown
    
     {{$NEXT}}
    
    +0.04 2013-09-30T11:03:15Z
    +
    +    - CONST() macro conflict with MinGW.
    +      (reported by mattn++)
    +
     0.03 2013-09-30T09:19:31Z
    
         - Custom handler support(Not documented yet).

--- hoedown/test/MarkdownTest_1.0.3/MarkdownTest.pl
+++ hoedown/test/MarkdownTest_1.0.3/MarkdownTest.pl
@@ -12,7 +12,7 @@ use warnings;
 use Getopt::Long;
 use Benchmark;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 # Sat 24 Dec 2005
 
 my $time_start = new Benchmark;
--- lib/Text/Markdown/Hoedown.pm
+++ lib/Text/Markdown/Hoedown.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use parent qw(Exporter);
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 our @EXPORT = qw(
     markdown
