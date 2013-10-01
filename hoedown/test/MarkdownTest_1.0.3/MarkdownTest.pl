85ad70b198feec663cc9f50f380269dd87217c30
tokuhirom <tokuhirom@gmail.com>
2013-10-01 22:12:38 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 86fc97e..092e50f 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Text-Markdown-Hoedown
    
     {{$NEXT}}
    
    +0.06 2013-10-01T13:11:59Z
    +
    +    - set __USE_MINGW_ANSI_STDIO=1 to let vsnprintf in mingw
    +      (charsbar)
    +    - specify the correct ret value.
    +
     0.05 2013-10-01T02:15:31Z
    
         - MSVC fix comes from hoedown upstream.

--- hoedown/test/MarkdownTest_1.0.3/MarkdownTest.pl
+++ hoedown/test/MarkdownTest_1.0.3/MarkdownTest.pl
@@ -12,7 +12,7 @@ use warnings;
 use Getopt::Long;
 use Benchmark;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 # Sat 24 Dec 2005
 
 my $time_start = new Benchmark;
--- lib/Text/Markdown/Hoedown.pm
+++ lib/Text/Markdown/Hoedown.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use parent qw(Exporter);
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 
 our @EXPORT = qw(
     markdown
